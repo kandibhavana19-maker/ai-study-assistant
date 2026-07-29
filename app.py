1a548c130b59e6fc1045f603b92b3bc13dfd0ffa
kandibhavana19-maker <kandibhavana19@gmail.com>
2026-07-29 17:46:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,7 @@
-print("Welcome to AI Study Assistant")
+username = input("Enter Username: ")
+password = input("Enter Password: ")
+
+if username == "admin" and password == "1234":
+    print("Login Successful")
+else:
+    print("Invalid Username or Password")
