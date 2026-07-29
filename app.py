efa18f876fafa485be4f4bef5b2321d0e8ada213
kandibhavana19-maker <kandibhavana19@gmail.com>
2026-07-29 18:05:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ if choice == "1":
     username = input("Enter Username: ")
     password = input("Enter Password: ")
 
-    if username == "admin" and password == "1234":
+    if username =="admin" and password=="1234":
         print("Login Successful")
     else:
         print("Invalid Username or Password")
