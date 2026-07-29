dabdd17c3f81c16bed2410b5dcd1d58ed4c7b015
kandibhavana19-maker <kandibhavana19@gmail.com>
2026-07-29 18:04:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,16 +8,13 @@ print("4. Exit")
 choice = input("Enter your choice: ")
 
 if choice == "1":
-    username = input("Enter Username:
- ")
-    password = input("Enter Password: 
-")
-if username == "admin" and password 
-=="1234":
+    username = input("Enter Username: ")
+    password = input("Enter Password: ")
+
+    if username == "admin" and password == "1234":
         print("Login Successful")
     else:
-        print("Invalid Username or 
-Password")
+        print("Invalid Username or Password")
 
 elif choice == "2":
     print("Add Notes Selected")
