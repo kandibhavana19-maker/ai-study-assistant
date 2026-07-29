7ce82e6d46634bb507ef2fac5dc12346b01189c6
kandibhavana19-maker <kandibhavana19@gmail.com>
2026-07-29 17:54:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,23 @@
-username = input("Enter Username: ")
-password = input("Enter Password: ")
+print("=== AI Study Assistant ===")
 
-if username == "admin" and password =="1234":
-    print("Login Successful")
+print("1. Login")
+print("2. Add Notes")
+print("3. View Notes")
+print("4. Exit")
+
+choice = input("Enter your choice: ")
+
+if choice == "1":
+    print("Login Selected")
+
+elif choice == "2":
+    print("Add Notes Selected")
+
+elif choice == "3":
+    print("View Notes Selected")
+
+elif choice == "4":
+    print("Goodbye!")
+
 else:
-    print("Invalid Username or Password")
+    print("Invalid Choice")
